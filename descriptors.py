d16f6ce47d4a5cd07be25b9e74c636734734066c
blaqhrse <blaqhrse@gmail.com>
2019-07-19 11:23:06 -0500

    new files

---
+++ descriptors.py
@@ -0,0 +1,37 @@
+import key # API Key stored on a different file
+import datetime
+import requests
+import xml.etree.cElementTree as ET
+
+host = '10.46.160.82'
+
+
+def hour_resource_monitor():
+    """
+    Gets dataplane CPU statistics for the last hour and prints output to a file.
+    """
+    output = requests.get('https://'+ host + '/api/?type=op&cmd=<show>'
+                          '<running><resource-monitor><hour><last>1</last>'
+                          '</hour></resource-monitor></running></show>'
+                          '&key='+key.pa_vm_a_key, verify=False)
+    data = output.text
+    root = ET.fromstring(data)
+    date = datetime.datetime.now()
+    utc_time = datetime.datetime.utcnow()
+
+    with open('packet_descriptors.txt', mode='a+') as f:
+        f.write('*****************************************' + '\n')
+        f.write(str(utc_time) + ':' + ' Time is in UTC' + '\n')
+        f.write('*****************************************' + '\n')
+        for elem in root.iter():
+            if elem.tag == 'resource-utilization':
+                stats_node = elem
+                for child in stats_node:
+                    for item in child:
+                        tag = item.tag
+                        text = item.text
+                        stats = tag + ' ' + ':' + ' ' + text + ' ' + '\n'
+                        f.write(stats)
+
+
+hour_resource_monitor()
---
+++ getapikey.py
@@ -0,0 +1,26 @@
+import key
+import requests
+
+host = '10.46.164.193' # change this ip to match your target
+
+def get_api_key():
+    """
+    Gets API key from firewall or panorama and prints the output
+    """
+    get_key = requests.get('https://' + host + '/api/?type=keygen&'\
+                 'user=admin&password=paloalto', verify=False)
+    print(get_key.text)
+
+
+def get_sys_info():
+    """
+    Gets system information. Using function to validate API key
+    """
+    output = requests.get('https://' + host + '/api/?type=op&cmd='\
+            '<show><system><info></info></system></show>&key='\
+            + key.pan_vm_key, verify=False)
+    print(output.text)
+
+
+# get_api_key()
+get_sys_info()
